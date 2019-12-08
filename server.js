279222a0ec157abf677cf915e6702079e9b114d3
Donald Tang <turazinyc@gmail.com>
2019-12-08 02:36:18 -0500

    Added cors, fixed static files bug, https redirect

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 require("dotenv").config();
 const connectDB = require("./config/db");
 const express = require("express");
-const cors = require("cors");
 const path = require("path");
-const compression = require("compression");
-const enforce = require("express-sslify");
 
 connectDB();
 const app = express();
 
 app.use(express.json({ extended: false }));
 
-app.use(cors());
-app.use(compression());
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/profile", require("./routes/api/profiles"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/groups", require("./routes/api/groups"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
@@ -22,11 +20,6 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/profile", require("./routes/api/profiles"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/groups", require("./routes/api/groups"));
 
 const PORT = process.env.PORT || 5000;
 
